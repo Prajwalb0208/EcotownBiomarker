fdc9bf81c695bfffab985f8caafdddebd4e2cf19
Prajwal B <prajwalb0208@gmail.com>
2025-08-21 16:26:00 +0530

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,8 @@ const allowedOrigins = [
   ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (e.g. curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST'],
+  origin: "*",
+  methods: ["GET", "POST"],
   credentials: true
 }));
 
